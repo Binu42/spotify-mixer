ea9421f5cb5c5874e1bb55be16afa1f5045556c0
Binu42 <kbinu42@gmail.com>
2020-10-17 22:53:41 +0530

    added spinner and clean up :+1:

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import theme from "./Theme";
+import GlobalStyles from "./GlobalStyles";
+import Spinner from './component/Common/Spinner/Spinner';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route path="/">
+              Hello
+            </Route>
+          </Switch>
+        </Suspense>
+      </MuiThemeProvider>
+    </Router>
   );
 }
 
