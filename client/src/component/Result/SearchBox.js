2ba68147105dffe98024214a14faa51f684ebecd
Binu42 <kbinu42@gmail.com>
2020-11-13 21:02:56 +0530

    folder remaining (playlist/result)

---
+++ client/src/component/Result/SearchBox.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, InputBase, IconButton, Box, Grid, Divider, Switch, Typography, Button, Hidden } from '@material-ui/core';
+import { MdSearch } from 'react-icons/md'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    background: 'rgba(0, 0, 0, 0.21)',
+    width: "100%"
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    color: 'rgba(255, 255, 255, 0.6)',
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.6)',
+  }
+}));
+
+export default function SearchBox() {
+  const classes = useStyles();
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = () => {
+    setIsChecked(prev => !prev);
+  }
+
+  return (
+    <Box pt={2}>
+      <Grid className={classes.search} direction="column" container justify="center" alignContent="center">
+        <Grid item xs={12} style={{ width: '100%' }}>
+          <Paper component="form" className={classes.root}>
+            <Hidden mdDown>
+              <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                <MdSearch className={classes.icon} />
+              </IconButton>
+            </Hidden>
+            <InputBase
+              className={classes.input}
+              placeholder={`Search by ${isChecked ? 'Artist' : 'Track'}`}
+              inputProps={{ 'aria-label': 'type in any song, playlists or artists' }}
+            />
+            <Divider className={classes.divider} orientation="vertical" />
+            <Typography component="div" color="secondary">
+              <Grid component="label" container alignItems="center" spacing={1}>
+                <Grid item>Artist</Grid>
+                <Grid item>
+                  <Switch size="small" checked={isChecked} onChange={handleChange} color="primary" name="artistSwitch" />
+                </Grid>
+                <Grid item>Track</Grid> |
+                <Button onClick={e => setIsChecked(false)} style={{ textTransform: "capitalize" }} color="secondary">Reset</Button>
+              </Grid>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
---
+++ client/src/component/Result/index.js
@@ -0,0 +1,26 @@
+import { Box, Container, Grid } from '@material-ui/core'
+import React from 'react'
+import SaveOnSpotify from './SaveOnSpotify'
+import SearchBox from './SearchBox'
+import TrackListSettings from './TrackListSettings'
+
+const Result = () => {
+  return (
+    <Container>
+      <SearchBox />
+      <Box py={2}>
+        <Grid container justify="center" spacing={1}>
+          <Grid item xs={12} md={9}>
+            lorem10000
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <SaveOnSpotify />
+            <TrackListSettings />
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  )
+}
+
+export default Result
