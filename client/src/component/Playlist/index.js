b4b6984c8f8526e0cc32ac0fc68b59ae4ef86c31
Binu42 <kbinu42@gmail.com>
2020-11-13 21:02:04 +0530

    my playlists

--- client/src/component/Playlist/index.js
+++ client/src/component/Playlist/index.js
@@ -1,26 +1,110 @@
-import { Box, Container, Grid } from '@material-ui/core'
-import React from 'react'
-import SaveOnSpotify from './SaveOnSpotify'
-import SearchBox from './SearchBox'
-import TrackListSettings from './TrackListSettings'
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import PlaylistCard from './PlaylistCard';
+import { Container, Grid, IconButton, InputBase, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { MdSearch } from 'react-icons/md';
+import { Redirect } from 'react-router-dom';
 
-const Playlist = () => {
+const transport = axios.create({
+  withCredentials: true,
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    background: 'rgba(0, 0, 0, 0.21)',
+    width: 600,
+    [theme.breakpoints.down('sm')]: {
+      width: 350
+    }
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    color: 'rgba(255, 255, 255, 0.6)',
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.6)',
+  }
+}))
+
+function Playlist() {
+  const classes = useStyles();
+  const [accessToken] = useState(Cookies.get('access_token'));
+  const [playlists, setPlaylists] = useState([]);
+  const [filteredPlaylists, setFilteredPlaylists] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (accessToken) {
+      let url = process.env.REACT_APP_API_URL + '/playlists';
+
+      transport.get(url).then(
+        (response) => {
+          setPlaylists(response.data.playlists);
+          setFilteredPlaylists(response.data.playlists);
+          setLoading(false);
+        }
+      );
+    }
+  }, []);
+
+  if (!accessToken) {
+    return <Redirect to="/" />;
+  }
+
+  const filter = (searchTerm) => {
+    setFilteredPlaylists(playlists.filter((item) => item.name.toLowerCase().includes(searchTerm.toLowerCase())));
+  };
+
   return (
     <Container>
-      <SearchBox />
-      <Box py={2}>
-        <Grid container justify="center" spacing={1}>
-          <Grid item xs={12} md={9}>
-            lorem10000
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <SaveOnSpotify />
-            <TrackListSettings />
-          </Grid>
+
+      <Typography variant="h4" style={{ color: "#fff", textAlign: 'center' }}> Select playlist </Typography>
+      <br />
+
+      <Grid container justify="center">
+        <Grid item>
+          <Paper component="form" className={classes.root}>
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+              <MdSearch className={classes.icon} />
+            </IconButton>
+            <InputBase
+              onChange={e => filter(e.target.value)}
+              className={classes.input}
+              placeholder="Type in any song, playlists or artists"
+              inputProps={{ 'aria-label': 'type in any song, playlists or artists' }}
+            />
+          </Paper>
         </Grid>
-      </Box>
+      </Grid>
+
+      <br />
+      <div>
+        <Grid container spacing={2} justify="center">
+          {loading && <>
+            <PlaylistCard loading={true} />
+            <PlaylistCard loading={true} />
+            <PlaylistCard loading={true} />
+          </>}
+          {filteredPlaylists &&
+            filteredPlaylists.map((item) => {
+              return <PlaylistCard playlist={item} key={item.id} />;
+            })}
+          {
+            filteredPlaylists && filteredPlaylists.length === 0 && <Typography variant="h5" style={{ color: "#fff", textAlign: 'center' }}>No playlist exist</Typography>
+          }
+        </Grid>
+      </div>
     </Container>
-  )
+  );
 }
 
-export default Playlist
+export default Playlist;
