9bc47e3460d7053b871eb3b6b4bd0c3d9843e343
Binu42 <kbinu42@gmail.com>
2020-11-15 02:36:51 +0530

    deploying to heroku

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ app.use(cors(corsOptions));
 app.use(express.static('build'));
 app.use('/api', api);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 app.get('*', function (req, res) {
   res.sendFile('./build/index.html', { root: __dirname });
